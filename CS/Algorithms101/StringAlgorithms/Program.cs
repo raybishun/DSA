40350def1c7e82ff1e15a76ff954849f175d1d6d
RayBishun <ray.bishun@bitsbytes.com>
2023-02-12 12:18:07 -0500

    Validation algorithm using All() and Any() from LINQ.

---
+++ CS/Algorithms101/StringAlgorithms/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace StringAlgorithms;
+class Program
+{
+    // Validation algorithm using All() and Any() from LINQ.
+    
+    static void Main(string[] args)
+    {
+        // Console.WriteLine(IsUpperCase("hello"));
+        // Console.WriteLine(IsUpperCase("HELLO"));
+        // Console.WriteLine(IsLowerCase("hello"));
+        // Console.WriteLine(IsLowerCase("HELLO"));
+
+        Console.WriteLine(IsPasswordComplex("P@ssw0rd"));
+    }
+
+    static bool IsUpperCase(string s)
+    {
+        // if(s.All(char.IsUpper))
+        // {
+        //     return true;
+        // }
+        // else
+        // {
+        //     return false;
+        // }
+        
+        return s.All(char.IsUpper);
+    }
+
+    static bool IsLowerCase(string s)
+    {
+        return s.All(char.IsLower);
+    }
+
+    static bool IsPasswordComplex(string s)
+    {
+        return s.Any(char.IsUpper) 
+            && s.Any(char.IsLower) 
+            && s.Any(char.IsDigit)
+            && s.Any(char.IsPunctuation);
+    }
+}
