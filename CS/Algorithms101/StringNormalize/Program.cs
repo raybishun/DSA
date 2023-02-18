a9a437d85178dc1519e869042a372be734700010
RayBishun <ray.bishun@bitsbytes.com>
2023-02-18 14:24:51 -0500

    Normalized strings.

---
+++ CS/Algorithms101/StringNormalize/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace StringNormalize;
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine(NormalizeString("Hello There, BUDDY!    "));
+    }
+
+    static string NormalizeString(string input)
+    {
+        return input.ToLower().Trim().Replace(",", "");
+    }
+}
